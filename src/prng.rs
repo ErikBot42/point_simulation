31acd627c6ba92857ab31970f36c2bc65e734805
Erik Magnusson <erik.arn.magnusson@gmail.com>
2023-06-17 16:05:07 +0200

    web support

---
+++ src/prng.rs
@@ -0,0 +1,16 @@
+use std::ops::Range;
+
+pub struct Prng(pub u64);
+
+impl Prng {
+    fn next(&mut self) -> u64 {
+        self.0 ^= self.0 << 13;
+        self.0 ^= self.0 >> 7;
+        self.0 ^= self.0 << 17;
+        self.0
+    }
+    pub fn f32(&mut self, r: Range<f32>) -> f32 {
+        ((self.next() as u32) as f32 / u32::MAX as f32) * (r.end - r.start) + r.start
+    }
+}
+
